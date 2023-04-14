dff21e79e8a6dc434f7092a4e7396d927ba37f22
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-04-15 03:08:55 +0530

    Format and Add readme

--- week-3/Calculator/Calculator.sol
+++ week-3/Calculator/Calculator.sol
@@ -4,48 +4,43 @@ pragma solidity >=0.8.17;
 
 contract Calculator {
     // take 2 integers and return addition
-    function addition(uint256 _num1, uint256 _num2)
-        public
-        pure
-        returns (uint256)
-    {
+    function addition(
+        uint256 _num1,
+        uint256 _num2
+    ) public pure returns (uint256) {
         return _num1 + _num2;
     }
 
     // take 2 integers and return subtraction
     // use int for holding negetive value
-    function subtraction(int256 _num1, int256 _num2)
-        public
-        pure
-        returns (int256)
-    {
+    function subtraction(
+        int256 _num1,
+        int256 _num2
+    ) public pure returns (int256) {
         return _num1 - _num2;
     }
 
     // take 2 integers and return multiplication
-    function multiplication(uint256 _num1, uint256 _num2)
-        public
-        pure
-        returns (uint256)
-    {
+    function multiplication(
+        uint256 _num1,
+        uint256 _num2
+    ) public pure returns (uint256) {
         return _num1 * _num2;
     }
 
     // take 2 integers and return division
-    function division(uint256 _num1, uint256 _num2)
-        public
-        pure
-        returns (uint256)
-    {
+    function division(
+        uint256 _num1,
+        uint256 _num2
+    ) public pure returns (uint256) {
         return _num1 / _num2;
     }
 
     // take 2 integers and return modulo
-    function modulo(uint256 _num1, uint256 _num2)
-        public
-        pure
-        returns (uint256)
-    {
-        return _num1**_num2;
+    function modulo(
+        uint256 _num1,
+        uint256 _num2
+    ) public pure returns (uint256) {
+        return _num1 ** _num2;
     }
 }
